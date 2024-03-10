5ef14f0ec7fb6f719deb03b3e75c546ebdf9a79a
anthonypaulbalducci <34378880+anthonypaulbalducci@users.noreply.github.com>
2024-03-09 19:29:13 -0500

    Create deepR.R
    Init commit

---
+++ deepR.R
@@ -0,0 +1,10 @@
+# DeepR (pronounced 'deeper') 
+# A minimal, light-weight L-layer Deep Learning Neural Network implemented in R. 
+
+init_param <- function(layer_dimensions) {
+}
+
+linear_forward_prop <- function(Activation, Weight, Intercept) {
+}
+
+
