61565b00cb758651c5c2056ddb68061052231624
anthonypaulbalducci <34378880+anthonypaulbalducci@users.noreply.github.com>
2024-03-19 01:28:19 -0400

    Update deepR.R

--- deepR.R
+++ deepR.R
@@ -1,22 +1,33 @@
 # DeepR (pronounced 'deeper') 
 # A minimal, light-weight L-layer Deep Learning Neural Network implemented in R. 
 
-init_param <- function(layer_dimensions) {
+initialize_parameters_deep <- function (layers_dims) {
+return (parameters)
+} 
 
-params <- c("W1"= W1, "b1" = b1, "W2"= W2)
-
-return (params)
+L_model_forward <- function (X, parameters) {
+list(AL, caches)
+return (AL, caches)
 }
 
-linear_forward_prop <- function(activation, weight, intercept) {
-a %*% b
+compute_cost <- function (AL, Y) {
+return (cost)
+
+L_model_backward <- function (AL, Y, caches) {
+return (grads)
 }
 
-cost <- function(layer_l_activation, Y) {
+update_parameters <- function(parameters, grads, learning_rate) {
+return (parameters)
 }
 
-linear_back_prop <- function() {
-}
+L_layer_model <- function (X, Y, layers_dims, learning_rate = 0.0075, num_iterations = 3000, print_cost=False) {
 
-activation_back <- function(dA, cache, activation) {
+# In R multiple returns must be concatenated as lists
+# and are referenced by: 
+# my_model <- L_layer_model(....)
+# my_model$parameters, etc
+
+temp <- list("parameters" = parameters, "costs" = costs)
+return (temp)
 }
